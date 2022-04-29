70ec138d85f2e1dd9c18cb393d2db9a703b2fa86
Дмитрий бохан <manager25@office.net>
2022-04-29 15:50:12 +0300

    семинар1 с#

---
+++ task0/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+Например:
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+Console.WriteLine("Введите число: ");
+string info = Console.ReadLine();
+
+Console.WriteLine(info);
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int rusult = number * number ;
+
+Console.WriteLine(rusult);
---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Напишите программу вычисления значения
+функции возведения числа в квадрат.
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+
+Console.WriteLine("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Number * Number);
